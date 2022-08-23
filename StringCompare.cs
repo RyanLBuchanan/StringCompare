065f343b7b7c0360d1bc996396436f3b674b1ee4
Ryan Buchanan <buchanan.ryan22@gmail.com>
2022-08-23 14:52:03 -0600

    feat: Finished

--- StringCompare.cs
+++ StringCompare.cs
@@ -10,37 +10,59 @@ namespace StringCompare
     {
         static void Main(string[] args)
         {
-            var string1 = "hello";
-            var string2 = "good bye";
-            var string3 = "Happy Birthday";
-            var string4 = "happy birthday";
+            //var string1 = "hello";
+            //var string2 = "good bye";
+            //var string3 = "Happy Birthday";
+            //var string4 = "happy birthday";
 
-            // Outut values of four strings
-            Console.WriteLine($"string1: \"{string1}\"" +
-                $"\nstring2: \"{string2}\"" +
-                $"\nstring3: \"{string3}\"" +
-                $"\nstring4: \"{string4}\"\n");
+            //// Outut values of four strings
+            //Console.WriteLine($"string1: \"{string1}\"" +
+            //    $"\nstring2: \"{string2}\"" +
+            //    $"\nstring3: \"{string3}\"" +
+            //    $"\nstring4: \"{string4}\"\n");
 
-            // Test for equality using Equals method
-            if (string1.Equals("hello"))
+            //// Test for equality using Equals method
+            //if (string1.Equals("hello"))
+            //{
+            //    Console.WriteLine("string1 equals\"hello\"");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("string1 does not equal \"hello\"");
+            //}
+
+            //// Test for equality with ==
+            //if (string1 == "hello")
+            //{
+            //    Console.WriteLine("string1 equals\"hello\"");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("string1 does not equal \"hello\"");
+            //}
+
+            /****************** StringStartEnd ********************/
+            string[] strings = { "started", "starting", "ended", "ending" };
+
+            // Test every string to see if it starts with "st"
+            foreach (var element in strings)
             {
-                Console.WriteLine("string1 equals\"hello\"");
-            }
-            else
-            {
-                Console.WriteLine("string1 does not equal \"hello\"");
+                if (element.StartsWith("st"))
+                {
+                    Console.WriteLine($"\"{element}\" starts with \"st\"");
+                }
             }
 
-            // Test for equality with ==
-            if (string1 == "hello")
-            {
-                Console.WriteLine("string1 equals\"hello\"");
-            }
-            else
-            {
-                Console.WriteLine("string1 does not equal \"hello\"");
-            }
+            Console.WriteLine();
 
+            // Test every string to see if it ends with "ed"
+            foreach (var element in strings)
+            {
+                if (element.EndsWith("ed"))
+                {
+                    Console.WriteLine($"\"{element}\" ends with\"ed\"");
+                }
+            }
         }
     }
 }
